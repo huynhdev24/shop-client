ea91e9454cc96d282d0db0758bcce062aead3a56
huynhdev24 <huynh.dev.24@gmail.com>
2023-04-23 00:06:21 +0700

    fix bugs

--- src/pages/Admin/History/HistoryList/index.js
+++ src/pages/Admin/History/HistoryList/index.js
@@ -1,21 +1,21 @@
 import { useCallback, useEffect, useState } from "react";
 import { Link } from "react-router-dom"
-import { toast } from 'react-toastify';
+// import { toast } from 'react-toastify';
 import PaginationBookStore from "../../../../components/PaginationBookStore";
 import { FaEdit, FaTrashAlt, FaSearch } from "react-icons/fa"
 
 
 import { Row, Col, Table, Spinner, Modal, Button } from "react-bootstrap";
-import bookApi from "../../../../api/bookApi";
+import historyApi from "../../../../api/historyApi";
 import format from "../../../../helper/format";
 
 function HistoryList() {
-  const [bookData, setBookData] = useState({});
+  const [historyData, setHistoryData] = useState({});
   const [page, setPage] = useState(1);
 
   const [loading, setLoading] = useState(false);
 
-  const [bookDelete, setBookDelete] = useState({})
+  const [historyDelete, setHistoryDelete] = useState({})
 
   const [showModal, setShowModal] = useState(false);
 
@@ -29,9 +29,10 @@ function HistoryList() {
         const query = {
           name: { "$regex": searchString, "$options": "$i" }
         }
-        const res = await bookApi.getAll({ query, page: page, limit: 10 });
+        const res = await historyApi.getAll({ query, page: page, limit: 5 });
+        console.log('history list: ' + JSON.stringify(res));
         setLoading(false);
-        setBookData({ books: res.data, totalPage: res.pagination.totalPage });
+        setHistoryData({ history: res.data, totalPage: res.pagination.totalPage });
       } catch (error) {
         setLoading(false);
         console.log(error);
@@ -45,32 +46,32 @@ function HistoryList() {
     setPage(page);
   }, []);
 
-  const handleCallApiDelete = async (e) => {
-    try {
-      const { data: orders } = await bookApi.checkIsOrdered(bookDelete._id)
-      if (orders.length > 0) {
-        toast.error('Sản phẩm đã được mua, không thể xóa!', {autoClose: 2000})
-        return
-      }
-      await bookApi.delete(bookDelete._id)
-      toast.success("Xóa thành công!", {autoClose: 2000})
-      setShowModal(false)
-      setBookData((preState) => {
-        const newArray = [...preState.books];
-        return {
-          ...preState,
-          books: newArray.filter((item) => item._id !== bookDelete._id)
-        }
-      });
-    } catch (error) {
-      alert("Xóa thất bại!")
-      setShowModal(false)
-    }
-  }
+  // const handleCallApiDelete = async (e) => {
+  //   try {
+  //     const { data: orders } = await bookApi.checkIsOrdered(bookDelete._id)
+  //     if (orders.length > 0) {
+  //       toast.error('Sản phẩm đã được mua, không thể xóa!', {autoClose: 2000})
+  //       return
+  //     }
+  //     await bookApi.delete(bookDelete._id)
+  //     toast.success("Xóa thành công!", {autoClose: 2000})
+  //     setShowModal(false)
+  //     setBookData((preState) => {
+  //       const newArray = [...preState.books];
+  //       return {
+  //         ...preState,
+  //         books: newArray.filter((item) => item._id !== bookDelete._id)
+  //       }
+  //     });
+  //   } catch (error) {
+  //     alert("Xóa thất bại!")
+  //     setShowModal(false)
+  //   }
+  // }
 
   return (
     <Row>
-      <Modal size="lg" show={showModal} onHide={() => setShowModal(false)}>
+      {/* <Modal size="lg" show={showModal} onHide={() => setShowModal(false)}>
         <Modal.Header closeButton>
           <Modal.Title>Xóa lịch sử</Modal.Title>
         </Modal.Header>
@@ -83,7 +84,7 @@ function HistoryList() {
             Xóa
           </Button>
         </Modal.Footer>
-      </Modal>
+      </Modal> */}
       <Col xl={12}>
         <div className="admin-content-wrapper">
           <div className="admin-content-header">Lịch sử Hệ thống</div>
@@ -107,8 +108,8 @@ function HistoryList() {
                   <th>STT</th>
                   <th>Hoạt động</th>
                   <th>Kiểu</th>
-                  <th>Tiêu đề</th>
-                  <th>Ngày</th>
+                  {/* <th>Tiêu đề</th> */}
+                  <th>Liên kết</th>
                   <th colSpan="2">Hành động</th>
                 </tr>
               </thead>
@@ -122,24 +123,24 @@ function HistoryList() {
                       />
                     </td>
                   </tr>
-                ) : bookData.books && bookData.books.length > 0 ? (
-                  bookData.books.map((item, index) => {
+                ) : historyData.history && historyData.history.length > 0 ? (
+                  historyData.history.map((item, index) => {
                     return (
                       <tr key={item._id}>
                         <td>{(1 && page - 1) * 10 + (index + 1)}</td>
                         <td className="text-start" style={{width: 500}}>
-                          {item.name} - {format.arrayToString(item.author || [])}
+                          {item.action}
                         </td>
                         <td>
-                          {format.arrayToString(item.genre || [])}
+                          {item.type}
                         </td>
-                        <td>
-                          {item.publisher?.name} - {item.year}
-                        </td>
-                        <td className="price">{format.formatPrice(item.price)}</td>
+                        {/* <td>
+                          {item.title}
+                        </td> */}
+                        <td>{item.link}</td>
                         <td>
                           <Link
-                            to={`/admin/book/update/${item._id}`}
+                            to={`/admin/history/update/${item._id}`}
                             className="btn btn-warning"
                             data-id={item._id}
                           >
@@ -150,7 +151,7 @@ function HistoryList() {
                           <button
                             className="btn btn-danger"
                             onClick={() => {
-                              setBookDelete(item)
+                              setHistoryDelete(item)
                               setShowModal(true)
                             }}
                           >
@@ -162,7 +163,7 @@ function HistoryList() {
                   })
                 ) : (
                   <tr>
-                    <td colSpan={7}>Không có sản phẩm nào!</td>
+                    <td colSpan={7}>Không có dòng lịch sử thao tác nào!</td>
                   </tr>
                 )}
               </tbody>
@@ -170,9 +171,9 @@ function HistoryList() {
             <div className="admin-content-pagination">
               <Row>
                 <Col xl={12}>
-                  {bookData.totalPage > 1 ? (
+                  {historyData.totalPage > 1 ? (
                     <PaginationBookStore
-                      totalPage={bookData.totalPage}
+                      totalPage={historyData.totalPage}
                       currentPage={page}
                       onChangePage={handleChangePage}
                     />
