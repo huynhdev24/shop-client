2112cc256ab31bd67fb03f7137d24734c382020a
huynhdev24 <huynh.dev.24@gmail.com>
2023-05-01 20:57:32 +0700

    FE: save url seaching

--- src/layouts/components/Search/SearchResultItem.js
+++ src/layouts/components/Search/SearchResultItem.js
@@ -1,8 +1,37 @@
 import { Link } from "react-router-dom";
 import styles from "./Search.module.css";
+
+//history
+import { useSelector } from "react-redux";
+import { useState } from "react";
+import historyApi from '../../../api/historyApi';
+
 export default function SearchResultItem({ data }) {
+
+  //handle click để lưu lại lịch sử
+  const { userId } = useSelector((state) => state.auth);
+  const [addHistory, setAddHistory] = useState({
+    action: 'Tìm sách' + data.name,
+    type: 'Tìm hiếm sách',
+    title: 'Tìm kiếm sách tại SmartShop',
+    link: 'http://localhost:3000/chi-tiet-san-pham/' + data.slug,
+    user: userId
+  })
+
+  const handleSubmitAdd = async (e) => {
+    e.preventDefault()
+    try {
+      await historyApi.create(addHistory)
+    } catch (error) {
+      setAddHistory()
+      console.log(error);
+    }
+  }
+  //end
+
   return (
-    <Link to={`/chi-tiet-san-pham/${data.slug}`}>
+    <div onClick={handleSubmitAdd}>
+      <Link to={`/chi-tiet-san-pham/${data.slug}`}>
       <div className={styles.resultItem}>
         <div className={styles.img}>
           <img src={data.imageUrl} alt="" />
@@ -13,5 +42,6 @@ export default function SearchResultItem({ data }) {
         </div>
       </div>
     </Link>
+    </div>
   );
 }
